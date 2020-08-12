c1aba3c15a17775add9acd098071bd43645746d0
takemo-pro <take.webengineer@gmail.com>
2020-08-12 13:27:25 +0900

    Edit Home and Help pages Make UsersController

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
-module ApplicationHelper
+module ApplicationHelper 
+    ##基本タイトルか基本タイトルにサブタイトルをつけたものを返します。
+    def set_full_title(page_title="")
+        base_title="Knowte"
+        if page_title.empty?
+            provide(:title, base_title) 
+        elsif 
+            provide(:title, page_title + " / " + base_title)
+        end
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  root 'home_pages#home'
+  get '/help', to: 'home_pages#help'
+  get '/signup', to: 'users#new'
+
 end
