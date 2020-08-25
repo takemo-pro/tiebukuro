ca79464eb72f18449f76d8cab1ec4db63b0d11d8
takemo-pro <take.webengineer@gmail.com>
2020-08-25 21:05:14 +0900

    Edit webpack/environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
 
+
+const path = require('path')
+const { link } = require('fs')
+
+//sass-loader
+const sassLoaders = environment.loaders.get('sass')
+const sassLoaderConfig = sassLoaders.use.filter(config => config.loader === 'sass-loader')[0]
+
+Object.assign(sassLoaderConfig.options,{
+  data:"@import 'global-imports.scss';",
+  includePaths:[path.resolve(__dirname,'../../../app/javascript/src/')]
+})
+
 module.exports = environment
