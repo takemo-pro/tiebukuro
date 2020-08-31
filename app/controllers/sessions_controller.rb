a473cb7180b401d977d47b41cfe7cd3cf8d35520
takemo-pro <take.webengineer@gmail.com>
2020-09-01 06:54:58 +0900

    Implement user login with remembering

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user&.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
-      log_in user
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       flash[:success] = 'ログインしました'
-      redirect_to user
+      redirect_to @user
     else
       #フラッシュエラーメッセージを表示、newを描画
       flash.now[:danger] = 'メールアドレスかパスワードが正しくありません。'
@@ -20,7 +21,7 @@ class SessionsController < ApplicationController
 
   def destroy
     #セッションと一時ユーザー変数を消してルートへ
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_813_070_338) do
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'password_digest'
-    t.index ['email'], name: 'index_users_on_email', unique: true
+ActiveRecord::Schema.define(version: 2020_08_30_115834) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.string "remember_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
+
 end
