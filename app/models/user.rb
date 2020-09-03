949949e412da4b5ede6afd7e0837595cec362e52
takemo-pro <take.webengineer@gmail.com>
2020-09-04 06:48:30 +0900

    Editting activation and CI test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
-  before_save { email.downcase! }
+  before_save :downcase_email
+  before_create :create_activation_digest
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   has_secure_password
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
@@ -31,4 +32,14 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+    def create_activation_digest #アクティベーションのトークンとダイジェストを生成、保存する
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
+
+    def downcase_email #メールアドレスをDB保存前に全て小文字化する（データの統一・唯一性の保持）
+      self.email.downcase!
+    end
 end
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 cd /var/www/rails/ && git pull
+bundle install --without development test
+bin/webpack
+rails db:migrate
+
+bundleo exec pumactl start
