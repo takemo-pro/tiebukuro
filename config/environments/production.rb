b60523e3aaa4765ab63d0e96cee61db0deec18c2
takemo-pro <take.webengineer@gmail.com>
2020-09-12 13:15:17 +0900

    Edit production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+
+  #master.keyがないときはエラーを吐くようにする
+  config.require_master_key = true
 end
