fe8528cb72c32b62ef053d565e56fc160372ab76
takemo-pro <take.webengineer@gmail.com>
2020-10-14 07:32:41 +0900

    Edit layout file

--- app/helpers/home_pages_helper.rb
+++ app/helpers/home_pages_helper.rb
@@ -2,13 +2,13 @@ module HomePagesHelper
   def sort_select_button_text
     case params[:sort]
     when "likes" then
-      text = "いいねが多い順"
+      text = "いいねが多い順の質問"
     when "follow" then
       text = "フォロー中の質問"
     when "nonsolved" then
       text = "未解決の質問"
     else
-      text = "新規投稿順"
+      text = "新規投稿順の質問"
     end
 
   end
