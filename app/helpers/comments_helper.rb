5886c1d1b79ec5215b824f34beb96c7fadac5e04
takemo-pro <take.webengineer@gmail.com>
2020-10-14 23:50:44 +0900

    Repair comment link

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,9 +1,9 @@
 module CommentsHelper
   #返信がついていなくてレス相手であれば返せる
   def correct_comment_user?(comment_parent) #コメント可能なユーザーか返す
-    if current_user.id == (comment_parent.reply_user_id) || current_user.id == comment_parent.question.user_id
+    if (current_user.id == (comment_parent.reply_user_id)) || (current_user == comment_parent.question.user)
       #返信先に誰も返信してなければ返信できる
-      !comment_parent.replies.any?
+      comment_parent.replies.empty?
       #解決済み質問にもコメできない
       if comment_parent.solved?
         return false
