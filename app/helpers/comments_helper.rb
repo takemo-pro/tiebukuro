7d9e2ccb8ddb834333dbf030b2c8ac1034146691
takemo-pro <take.webengineer@gmail.com>
2020-10-15 06:39:41 +0900

    Repair header css

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -3,7 +3,9 @@ module CommentsHelper
   def correct_comment_user?(comment_parent) #コメント可能なユーザーか返す
     if (current_user.id == (comment_parent.reply_user_id)) || (current_user == comment_parent.question.user)
       #返信先に誰も返信してなければ返信できる
-      comment_parent.replies.empty?
+      if comment_parent.replies.empty?
+        return true
+      end
       #解決済み質問にもコメできない
       if comment_parent.solved?
         return false
