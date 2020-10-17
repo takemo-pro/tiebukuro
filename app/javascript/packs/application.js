39abf3650daa666d1f3a7867c249648c3ddc7772
takemo-pro <take.webengineer@gmail.com>
2020-10-17 21:30:40 +0900

    Add bootstrap-material-design

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,13 +11,12 @@ require("jquery")
 import '../javascripts/application'
 
 //Import bootstrap file
-import "bootstrap"
-import "../src/application.scss"
-
+import "bootstrap-material-design"
 //Import fontawesome file
 import '@fortawesome/fontawesome-free/js/all'
 
 
+import "../src/application.scss"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
